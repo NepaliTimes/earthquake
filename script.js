0614c48125adcfba89cb6bb4896abffb99191bbd
NepaliTimes <nepalitimes.nt@gmail.com>
2025-04-25 17:53:26 +0545

    pause2

--- script.js
+++ script.js
@@ -1016,7 +1016,7 @@ document.addEventListener("click", () => {
 
 window.addEventListener('scroll', () => {
 	if (bee){
-		vid.paused = true;
+		vid.pause();
 		currentParentPos = parentList[bigbox].rotation.y;
 		if(vid.paused && fall.style.display == 'none' && fall2.style.display == 'none' && fall3.style.display == 'none' && fall4.style.display == 'none' && fall5.style.display == 'none' && fall6.style.display == 'none' && fall7.style.display == 'none' && fall8.style.display == 'none' && fall9.style.display == 'none' && fall10.style.display == 'none' && fall11.style.display == 'none' ){
 			console.log('modelabouttomove')
